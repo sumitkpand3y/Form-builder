a98d90d5e2bd02ce7fdc91b706580e9172868f49
sumittriofi <sumitkumar.p@flyerssoft.com>
2024-03-25 13:46:37 +0530

    Add client app

--- Server/src/config/config.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-
-export let config ={
-    serviceName: process.env.SERVICE_NAME || "API_SERVICE",
-    mongodbURI:process.env.MONGO_DB_URI || "mongodb://localhost:27017/GenericApi",
-    port:process.env.PORT || 7050,
-    dynamicModels:new Array(),
-}
